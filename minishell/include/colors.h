2d5e34c5124a3f60fc898729efcd5f74c857abbf
Toma Gajdov <tgajdov@c2r8s3.42lausanne.ch>
2024-09-24 21:21:33 +0200

    Creation du makefile et des headers colors.h et minishell.h

---
+++ minishell/include/colors.h
@@ -0,0 +1,38 @@
+// colors.h
+#ifndef COLORS_H
+# define COLORS_H
+
+// Reset
+# define C_RESET   "\x1b[0m"
+
+// Regular Colors
+# define BLACK   "\x1b[30m"
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define WHITE   "\x1b[37m"
+
+// Bold
+# define B_BLACK   "\x1b[1;30m"
+# define B_RED     "\x1b[1;31m"
+# define B_GREEN   "\x1b[1;32m"
+# define B_YELLOW  "\x1b[1;33m"
+# define B_BLUE    "\x1b[1;34m"
+# define B_MAGENTA "\x1b[1;35m"
+# define B_CYAN    "\x1b[1;36m"
+# define B_WHITE   "\x1b[1;37m"
+
+// Background
+# define BG_BLACK   "\x1b[40m"
+# define BG_RED     "\x1b[41m"
+# define BG_GREEN   "\x1b[42m"
+# define BG_YELLOW  "\x1b[43m"
+# define BG_BLUE    "\x1b[44m"
+# define BG_MAGENTA "\x1b[45m"
+# define BG_CYAN    "\x1b[46m"
+# define BG_WHITE   "\x1b[47m"
+
+#endif // COLORS_H
---
+++ minishell/include/minishell.h
@@ -0,0 +1,20 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "colors.h"
+
+# define DEBUG(str) printf(B_RED"%s, line: %d\n"B_BLACK"%s\n\n"C_RESET, __FILE__, __LINE__, str);
+
+#endif
